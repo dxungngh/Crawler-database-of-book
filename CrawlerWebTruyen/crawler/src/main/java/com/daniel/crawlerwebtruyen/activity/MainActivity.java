cef9fcefa6d9b21e10970aa8d4fe4fe1657cf5c9
Daniel Nguyen <dxungngh@gmail.com>
2014-10-16 15:44:53 +0700

    add permission

--- CrawlerWebTruyen/crawler/src/main/java/com/daniel/crawlerwebtruyen/activity/MainActivity.java
+++ CrawlerWebTruyen/crawler/src/main/java/com/daniel/crawlerwebtruyen/activity/MainActivity.java
@@ -2,14 +2,42 @@ package com.daniel.crawlerwebtruyen.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 import com.daniel.crawlerwebtruyen.R;
+import com.daniel.crawlerwebtruyen.database.table.Book;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private final String BOOK_LINK = "http://webtruyen.com/buong-tay-toi-khong-lay-chong/";
+
+    private Book mBook;
+    private Document mDocument;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBook = new Book();
+        getDocument();
+        getDetailOfBook();
+    }
+
+    private void getDocument() {
+        try {
+            mDocument = Jsoup.connect(BOOK_LINK).get();
+        } catch (Exception e) {
+            Log.e(TAG, "getDocument", e);
+            getDocument();
+        }
+    }
+
+    private void getDetailOfBook() {
+
     }
 }

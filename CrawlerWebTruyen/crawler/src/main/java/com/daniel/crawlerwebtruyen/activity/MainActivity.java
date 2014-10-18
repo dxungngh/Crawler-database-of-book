aa054be193cc1084556703384cd2a448544446dd
Daniel Nguyen <dxungngh@gmail.com>
2014-10-18 21:44:19 +0700

    get link and name of chapters

--- CrawlerWebTruyen/crawler/src/main/java/com/daniel/crawlerwebtruyen/activity/MainActivity.java
+++ CrawlerWebTruyen/crawler/src/main/java/com/daniel/crawlerwebtruyen/activity/MainActivity.java
@@ -11,6 +11,7 @@ import com.daniel.crawlerwebtruyen.database.table.Book;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,9 +20,11 @@ public class MainActivity extends ActionBarActivity {
     private final int TIMEOUT = 100000;
     private final String USER_AGENT = "Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6";
     private final String REFERRER = "http://www.google.com";
+    private final String PAGE_LINK = "http://webtruyen.com/story/Paging_listbook/5588/";
 
     private Book mBook;
     private Document mDocument;
+    private boolean mIsDone = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,44 @@ public class MainActivity extends ActionBarActivity {
 
         mBook = new Book();
         new CrawlBookTask().execute();
+    }
+
+    private void getChapterFromRow(Element rowElement) {
+        try {
+            Elements tdElements = rowElement.select("td");
+            String name = tdElements.get(3).text();
+            String link = tdElements.get(3).select("a").first().attr("href");
+            Log.i(TAG, name + " -- " + link);
+        } catch (Exception e) {
+            Log.e(TAG, "getChapterFromRow", e);
+        }
+    }
+
+    private void getChaptersOfPage(String pageLink) {
+        try {
+            Log.i(TAG, pageLink);
+            Document pageDocument = Jsoup.connect(pageLink)
+                .userAgent(USER_AGENT)
+                .referrer(REFERRER)
+                .timeout(TIMEOUT)
+                .get();
+            if (pageDocument == null) {
+                mIsDone = true;
+                return;
+            }
+            Elements rowElements = pageDocument.select(".gridlistchapter").first().select("tbody").first().select("tr");
+            if (rowElements == null || rowElements.size() <= 1) {
+                mIsDone = true;
+                return;
+            }
+            for (int i = 1; i < rowElements.size(); i++) {
+                Element rowElement = rowElements.get(i);
+                getChapterFromRow(rowElement);
+            }
+        } catch (Exception e) {
+            mIsDone = true;
+            Log.e(TAG, "getChaptersOfPage", e);
+        }
     }
 
     private void getDocument() {
@@ -49,6 +90,7 @@ public class MainActivity extends ActionBarActivity {
     private void getDetailOfBook() {
         getInformationOfBook();
         getOverviewOfBook();
+        getListOfChapters();
 
         Log.i(TAG, mBook.toString());
     }
@@ -72,6 +114,16 @@ public class MainActivity extends ActionBarActivity {
         mBook.setStatus(status.trim());
     }
 
+    private void getListOfChapters() {
+        for (int i = 0; i < 1000; i++) {
+            String pageLink = PAGE_LINK + i;
+            getChaptersOfPage(pageLink);
+            if (mIsDone) {
+                break;
+            }
+        }
+    }
+
     private void getOverviewOfBook() {
         Element element = mDocument.select(".mota").first();
         mBook.setOverview(element.html());
@@ -81,12 +133,12 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected Void doInBackground(Void... unusedParams) {
             getDocument();
+            getDetailOfBook();
             return null;
         }
 
         @Override
         protected void onPostExecute(Void unusedParam) {
-            getDetailOfBook();
         }
     }
 }
